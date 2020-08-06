8a2e8f66bee552a7a48877fa32298abffae04722
Marcin <jerzakm@gmail.com>
2020-08-06 21:53:13 +0200

    layout sketching

--- src/App.svelte
+++ src/App.svelte
@@ -6,34 +6,40 @@
 
 </style>
 
-<main>
+<main class="flex">
 
-  <logo class="text-4xl mt-24">jerzakm.com</logo>
+  <logo class="text-2xl mt-4 ml-4">
+    <h1>jerzakm.com</h1>
+  </logo>
 
-  <header>
-    <h1>Marcin Jerzak</h1>
-    <h2>software developer</h2>
-    <span>
-      web & cloud solutions - creative applications - business intelligence
-    </span>
-  </header>
+  <div class="flex-1">
+    <header>
+      <h1>Marcin Jerzak</h1>
+      <h2>software developer</h2>
+      <span>
+        web & cloud solutions - creative applications - business intelligence
+      </span>
+    </header>
 
-  <contact>
-    <span>Github</span>
-    <span>LinkedIn</span>
-    <span>dev.to</span>
-    <h1>Connect & talk to me.</h1>
-  </contact>
+    <contact>
+      <span>Github</span>
+      <span>LinkedIn</span>
+      <span>dev.to</span>
+      <h1>Connect & talk to me.</h1>
+    </contact>
 
-  <about>
-    <span>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur esse
-      obcaecati, facilis sit autem vitae optio dolores a mollitia incidunt amet
-      veniam, unde quos sapiente rem earum aperiam fugit dolor.
-    </span>
-  </about>
+    <about>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur esse
+        obcaecati, facilis sit autem vitae optio dolores a mollitia incidunt
+        amet veniam, unde quos sapiente rem earum aperiam fugit dolor.
+      </span>
+    </about>
+  </div>
 
-  <projects>
+  <filler class="flex-1" />
+
+  <projects class="flex-1">
     <project>
       <h2>Project name</h2>
       <span>Project desc</span>
