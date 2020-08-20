8f941ae7109cf7628c95a88c9c1f96752f88c499
Marcin <jerzakm@gmail.com>
2020-08-20 18:42:24 +0200

    basic layout responsiveness

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,11 @@
   import { onMount } from 'svelte'
   import { physicsDomInit } from './stores'
   import { initPhysicsWorld } from './physics'
+
+  let windowWidth = window.innerWidth
+  window.addEventListener('resize', () => {
+    windowWidth = window.innerWidth
+  })
 </script>
 
 <style>
@@ -17,23 +22,25 @@
   }
 </style>
 
-<main class="p-24 h-full w-full flex flex-row">
+<main class="flex flex-col sm:flex-row p-10 lg:p-24 h-full w-full ">
   <about class="flex-1 flex flex-col">
-    <h1 class="leading-none">Hello!</h1>
-    <h2 class="leading-tight">My name is Marcin and I code stuff</h2>
-    <div class="mt-2 text-xl text-justify">
+    <h1 class="leading-none 2xl:text-5xl">Hello!</h1>
+    <h2 class="leading-tight 2xl:text-4xl">
+      My name is Marcin and I code stuff
+    </h2>
+    <div class="mt-2 text-xl 2xl:text-3xl text-justify">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum,
       doloremque. Dignissimos temporibus laudantium, ipsa vitae dolore neque
       architecto consequatur iste velit atque vel, saepe nesciunt, enim fuga
       impedit rerum excepturi inventore.
     </div>
     <divider class="divider mt-4 mb-8" />
-    <h3 class="font-bold">Check out some of my work:</h3>
+    <h3 class="font-bold 2xl:text-4xl">Check out some of my work:</h3>
     {#each { length: 5 } as entry, i}
       <project class="mt-6">
         <span class="project-number">{`${i < 9 ? '0' : ''}${i + 1}`}</span>
         <description class="">
-          <div class="text-justify">
+          <div class="text-justify 2xl:text-2xl">
             <span class="font-bold">Marketflow</span>
             is an app for monitoring costs, profitability and other KPIs on
             allegro.pl marketplace.
@@ -47,20 +54,27 @@
       </project>
     {/each}
   </about>
-
-  <filler class="flex-1" />
-  <contact class="flex-1 flex flex-col max-w-xl items-end justify-between">
+  <filler class="xl:flex-1 p-4 xl:p-0" />
+  <contact
+    class="flex-1 flex flex-col max-w-xl items-end justify-between mt-16 lg:mt-0
+    mb-16 lg:mb-0">
     <div class="mr-64 text-xl">
-      <div class="get-physical bg-tealime heartbeat absolute">
-        <span class="opacity-0">Let's get physical</span>
-      </div>
-      <button class="absolute font-bold">Let's get physical</button>
+      {#if windowWidth > 1360}
+        <div class="get-physical bg-tealime heartbeat absolute">
+
+          <span class="opacity-0 2xl:text-3xl">Let's get physical</span>
+
+        </div>
+        <button class="absolute font-bold 2xl:text-3xl">
+          Let's get physical
+        </button>
+      {/if}
     </div>
     <div class="max-w-md">
-      <h3 class="font-bold">
+      <h3 class="font-bold 2xl:text-4xl">
         Let's connect and build something great together
       </h3>
-      <ul class="text-lg mt-4">
+      <ul class="text-lg mt-4 2xl:text-3xl">
         <li>
           email me at
           <a href="#">mail@gmail.com</a>
@@ -70,7 +84,25 @@
           <a href="#">here</a>
           or
         </li>
-        <li>check out my dev socials</li>
+        <!-- <li>check out my dev socials</li> -->
+        <li class="flex">
+          <img
+            src="icons/github.svg"
+            alt="Github profile"
+            class="w-6 2xl:w-12 mr-4 2xl:mr-8" />
+          <img
+            src="icons/linkedin.svg"
+            alt="LinkedIn profile"
+            class="w-6 2xl:w-12 mr-4 2xl:mr-8" />
+          <img
+            src="icons/dev-dot-to.svg"
+            alt="dev.to profile"
+            class="w-6 2xl:w-12 mr-4 2xl:mr-8" />
+          <img
+            src="icons/twitter.svg"
+            alt="Twitter profile"
+            class="w-6 2xl:w-12 mr-4 2xl:mr-8" />
+        </li>
       </ul>
     </div>
   </contact>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,33 @@ module.exports = {
     extend: {
       colors: {
         tealime: '#6affbf'
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '1024px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1280px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1600px',
+        '2xl': '2260px'
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '3.25rem',
+        '5xl': '5rem',
+        '6xl': '8rem',
+        '7xl': '12rem',
       }
     }
   },
