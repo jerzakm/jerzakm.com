5a2c76a5e0bbfca84ad5d756e387679f6d1a1981
Marcin Jerzak <jerzakm@gmail.com>
2021-02-15 23:04:40 +0100

    pre-tailwind

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,9 @@
+<style lang="scss">
+</style>
+
+<ul>
+  <li><a href=".">Home</a></li>
+  <li><a href=".">Articles</a></li>
+  <li><a href=".">Work</a></li>
+  <li><a href=".">Content</a></li>
+</ul>
--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -1,20 +1,30 @@
 <script>
   export let templateHtml, settings;
+
+  import Navigation from '../components/Navigation.svelte';
 </script>
 
 <style lang="scss">
-  @import '../variables.scss';
+  @import '../styles/variables.scss';
 
   :global(body) {
     background-color: $darker;
+    color: $light;
     padding: 0;
     margin: 0;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
   }
 </style>
 
 <svelte:head>
   <link rel="stylesheet" href="/style.css" />
 </svelte:head>
-<div class="container">
+
+<main>
+  <Navigation />
   {@html templateHtml}
-</div>
+</main>
